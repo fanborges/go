54af0e7490cf725d1052341b262719a84dd7ad63
fanborges <fanborges@gmail.com>
2018-03-26 23:41:56 -0600

    add helloapi

---
+++ helloapi/helloapi.go
@@ -0,0 +1,22 @@
+package main
+import
+(
+    "net/http"
+    "encoding/json"
+    "fmt"
+)
+type API struct {
+    Message string "json:message"
+}
+
+func main() {
+    http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+    message := API{"Hello, world!"}
+    output, err := json.Marshal(message)
+    if err != nil{
+            fmt.Println("Something went wrong!")
+    }
+      fmt.Fprintf(w, string(output))
+    })
+	http.ListenAndServe(":8080",nil)
+}
